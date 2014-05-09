2395bffa5b205c87d7dc88b548c9bced6839b2d6
Lauren DeVries <devrieslauren1@gmail.com>
2014-05-10 00:29:45 +1000

    Up to 9.2.3 Friendly forwarding

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -60,8 +60,25 @@ describe "Authentication" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
-      end #end of in the USers controller
+      end #end of in the Users controller
+
+       describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting a GET request to the Users#edit action" do
+        before { get edit_user_path(wrong_user) }
+        specify { expect(response.body).not_to match(full_title('Edit user')) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
     end# end of authorization
-  end # end describe signin do
+  end # describe as wrong user
+   end # end describe signin do
   end #end of Authentication
 end #end ofspec-helper
