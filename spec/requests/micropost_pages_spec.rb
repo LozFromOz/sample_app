3d22a96ed5b80bb1e4eb49790c6fd10e5adc4a0d
Lauren DeVries <devrieslauren1@gmail.com>
2014-05-11 17:50:25 +1000

    Add user microposts

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -28,6 +28,17 @@ describe "Micropost pages" do
       it "should create a micropost" do
         expect { click_button "Post" }.to change(Micropost, :count).by(1)
       end
+    end#of with valid information
+  end#of micropost creation
+    describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
     end
   end
-end
+end#of micropost pages
